94e910b901fdcba7fa18eb7a2b643a606a231c3a
oybekjon94 <x.oybek94@gmail.com>
2023-02-14 11:19:43 +0900

    [feat::] Add 1 HelloWorld

---
+++ src/javaBasics/HelloWorld.java
@@ -0,0 +1,11 @@
+package javaBasics;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //The main is a method. A method is a piece of code created to do a specific job.
+        //This static method can be called without the need to create an instance of the Java class.
+        //The void keyword states that the method does not return a value.
+        //the public keyword makes the main method available to the outer world without restrictions.
+    }
+}
---
+++ src/javaBasics/Readline.java
@@ -0,0 +1,4 @@
+package javaBasics;
+
+public class Readline {
+}
