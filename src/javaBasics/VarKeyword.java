d90fa5256cb692cc15523374681be8513009aa8e
oybekjon94 <x.oybek94@gmail.com>
2023-02-14 16:18:50 +0900

    [feat::] Add 6 var

---
+++ src/javaBasics/VarKeyword.java
@@ -0,0 +1,10 @@
+package javaBasics;
+
+public class VarKeyword {
+    public static void main(String[] args){
+        var name = "Oybek";
+        var age = 28;
+
+        System.out.println(name + " is " + age + " years old");
+    }
+}
