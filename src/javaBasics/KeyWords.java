e85f0cf21507f2fc90ab4b8af04c649fae6ce2e4
oybekjon94 <x.oybek94@gmail.com>
2023-02-14 17:12:16 +0900

    [feat::] Add 11 keywords

---
+++ src/javaBasics/KeyWords.java
@@ -0,0 +1,10 @@
+package javaBasics;
+
+public class KeyWords {
+    //The package,public,class,static,void,int,for tokens are Java keywords.
+    public static void main(String[] args){
+        for (int i = 0; i <= 5; i ++){
+            System.out.println(i);
+        }
+    }
+}
