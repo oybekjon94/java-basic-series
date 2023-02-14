602d2de3947de331cc3e07f47928d65b4ca382f4
oybekjon94 <x.oybek94@gmail.com>
2023-02-14 16:25:24 +0900

    [feat::] Add 7 constants

---
+++ src/javaBasics/Constants.java
@@ -0,0 +1,17 @@
+package javaBasics;
+
+public class Constants {
+    public static void main(String[] args){
+        final int WIDTH = 100;
+        final int HEIGHT = 150;
+        int var = 40;
+
+        var = 50;
+
+        //WIDTH = 110
+
+        System.out.println(WIDTH);
+        System.out.println(HEIGHT);
+        System.out.println(var);
+    }
+}
