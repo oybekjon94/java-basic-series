27d93e8cf81ee2f1bd94900552c268e53bfa8ee9
oybekjon94 <x.oybek94@gmail.com>
2023-02-15 12:06:51 +0900

    [feat::] Add javaBasics

---
+++ src/javaBasics2/SignOperator.java
@@ -0,0 +1,15 @@
+package javaBasics2;
+
+public class SignOperator {
+    public static void main(String[] args){
+        System.out.println(2);
+        System.out.println(+2);
+        System.out.println(-2);
+
+        //-----------------
+
+        int a = 1;
+        System.out.println(-a);
+        System.out.println(-(-a));
+    }
+}
